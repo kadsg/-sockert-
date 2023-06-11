81a4fce3fdaddcd5b569744dc476972c1223bf25
MingJie Li <kadsg0769@163.com>
2023-06-11 20:08:05 +0800

    修改代码注释

--- server/server.py
+++ server/server.py
@@ -28,7 +28,7 @@ class Server:
                 filename = self.business.receive_file_name(client_socket)  # 接收文件名
                 file_path = os.path.join(path, filename)  # 设置保存到服务器上的临时文件路径
                 self.business.receive_file(client_socket, file_path)  # 接收并写入客户端发送的文件
-                compressed_file_path = self.business.compress_file(file_path)  # 压缩文件并返回压缩后的文件路径以及文件类型过滤器
+                compressed_file_path = self.business.compress_file(file_path)  # 压缩文件并返回压缩后的文件路径
                 self.business.send_compressed_file(client_socket, compressed_file_path)  # 向客户端发送压缩后的文件
 
                 # 删除服务器临时文件夹

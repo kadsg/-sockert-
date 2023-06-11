075c145d6bc53384f69469ad0ab664dfcf8e1370
MingJie Li <kadsg0769@163.com>
2023-06-11 15:47:49 +0800

    支持连续收发文件

--- client/client.py
+++ client/client.py
@@ -6,64 +6,80 @@ import os
 host = 'localhost'  # Server的IP地址或主机名
 port = 8000  # Server监听的端口号
 
-# 创建socket对象
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 连接Server
-client_socket.connect((host, port))
+isConnected = False
 
-# 打开文件选择对话框
-print("连接成功！选择你要压缩的文件...")
-root = tkinter.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
-
-# 发送文件名
-filename = os.path.basename(file_path)
-client_socket.sendall(filename.encode())
-
-# 打开文件并发送
-with open(file_path, 'rb') as f:
-    length = 0
-    chunk = b''
-    while True:
-        # 一次读取1024字节的数据
-        data = f.read(1024)
-        # 追加到chunk中
-        chunk += data
-        length += len(data)
-        if not data:  # 读完了文件，退出循环
-            print(f"{length} 字节的数据将发送至客户端...")
-            client_socket.sendall(chunk)  # 发送数据
-            # 关闭写入通道，告诉Server数据已经发送完毕
-            client_socket.shutdown(socket.SHUT_WR)
-            break
-
-# 接收Server返回的数据
-compressed_data = b""
 while True:
-    data = client_socket.recv(1024)
-    if not data:  # 数据接收完毕，退出循环
+    print("选择你要压缩的文件...")
+    root = tkinter.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()  # 获取用户选择的文件路径
+    try:
+        sendFlag = False
+        # 打开文件并发送
+        with open(file_path, 'rb') as f:
+            # 如果连接没有建立
+            if not isConnected:
+                # 创建socket对象
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((host, port))
+                isConnected = True
+
+            # 首先发送文件名，让server创建临时文件
+            if not sendFlag:
+                # 发送文件名
+                filename = os.path.basename(file_path)
+                client_socket.sendall(filename.encode())
+                sendFlag = True
+
+            length = 0
+            chunk = b''
+            while True:
+                # 一次读取1024字节的数据
+                data = f.read(1024)
+                # 追加到chunk中
+                chunk += data
+                length += len(data)
+                if not data:  # 读完了文件，退出循环
+                    print(f"{length} 字节的数据将发送至客户端...")
+                    client_socket.sendall(chunk)  # 发送数据
+                    # 关闭写入通道，告诉Server数据已经发送完毕
+                    client_socket.shutdown(socket.SHUT_WR)
+                    break
+
+        # 接收Server返回的数据
+        compressed_data = b""
+        while True:
+            data = client_socket.recv(1024)
+            if not data:  # 数据接收完毕，退出循环
+                break
+            compressed_data += data
+
+        # 打开文件保存对话框
+        root = tkinter.Tk()
+        root.withdraw()
+        # 弹出保存文件对话框，获取用户选择的保存路径和文件名
+        # 文件类型过滤器
+        options = {
+            'defaultextension': '.zip',
+            'filetypes': [('ZIP files', '*.zip'), ('All files', '*.*')],
+        }
+        save_filename = tkinter.filedialog.asksaveasfilename(**options)
+
+        # 保存压缩数据到文件
+        with open(save_filename, "wb") as f:
+            f.write(compressed_data)
+            f.close()
+            print("文件保存成功！")
+    except FileNotFoundError:
+        print("操作取消！")
+
+    # 询问用户是否继续
+    client_socket.close()
+    isConnected = False
+    choice = input("是否继续？(y/n)")
+    if choice == 'n':
         break
-    compressed_data += data
-
-# 打开文件保存对话框
-root = tkinter.Tk()
-root.withdraw()
-# 弹出保存文件对话框，获取用户选择的保存路径和文件名
-# 文件类型过滤器
-options = {
-    'defaultextension': '.zip',
-    'filetypes': [('ZIP files', '*.zip'), ('All files', '*.*')],
-}
-save_filename = tkinter.filedialog.asksaveasfilename(**options)
-
-try:
-    # 保存压缩数据到文件
-    with open(save_filename, "wb") as f:
-        f.write(compressed_data)
-        f.close()
-except FileNotFoundError:
-    print("用户取消保存！")
+    continue
 
 # 关闭socket连接
 client_socket.close()
--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 import os
+import shutil
 import socket
 import zipfile
 
-host = ''  # 监听所有可用的网络接口
+host = ''  # 监听的IP地址，为空则表示监听本机所有IP
 port = 8000  # 监听的端口号
 
 # 创建socket对象
@@ -17,49 +18,49 @@ print('开始监听...')
 while True:
     # 接收Client的连接请求
     client_socket, addr = server_socket.accept()
-    print(f'收到来自：{addr} 的请求')
+    print(f'收到来自：{addr} 的请求！')
 
     try:
         # 接收Client发送的文件名
         filename = client_socket.recv(1024).decode()
-        path = 'server'
+        path = f'serverFor/{addr}'  # 临时文件夹路径
 
+        # 如果临时文件夹不存在，则创建
         if not os.path.exists(path):
             os.makedirs(path)
 
         # 接收Client发送的数据并写入文件（暂存至服务器）
         with open(os.path.join(path, filename), 'wb') as f:
-            data = b''
-            while True:
-                chunk = client_socket.recv(1024)
+            data = b''  # 用于存储接收到的数据
+            while True:  # 循环接收数据
+                chunk = client_socket.recv(1024)  # 一次接收1024字节的数据
                 # 如果接收到的数据为空，则退出循环
                 if not chunk:
                     print(f'接收完毕，共接收{len(data)}字节的数据！')
                     break
-                data += chunk
-            f.write(data)
-            f.close()
+                data += chunk  # 将接收到的数据追加到data中
+            f.write(data)  # 将data写入文件
+            f.close()  # 关闭文件
 
         # 压缩文件
         # 创建压缩文件对象
         compressed_data = zipfile.ZipFile(os.path.join(path, filename + '.zip'), mode='w',
                                           compression=zipfile.ZIP_DEFLATED)
-        # 将暂存在服务器上的文件写入压缩文件
-        compressed_data.write(os.path.join(path, filename))
-        compressed_data.close()
+        compressed_data.write(os.path.join(path, filename))  # 将暂存在服务器上的文件写入压缩文件
+        compressed_data.close()  # 关闭压缩文件对象
 
         # 读取压缩后的数据，并发送给Client
         with open(os.path.join(path, filename + '.zip'), 'rb') as f:
             compressed_data = f.read()
             client_socket.sendall(compressed_data)
             f.close()
-            # 删除服务器临时文件
-            os.remove(os.path.join(path, filename + '.zip'))
-            os.remove(os.path.join(path, filename))
 
-        # 关闭socket连接
-        client_socket.close()
-        print(f'服务完毕，已断开对{addr}的连接！')
+            # 删除服务器临时文件夹
+            shutil.rmtree(path)
 
-    except ConnectionResetError as e:
+        # 关闭Client连接
+        client_socket.shutdown(socket.SHUT_WR)
+        print(f'为客户端{addr}的请求提供服务完毕！')
+
+    except ConnectionResetError as e:  # 如果连接被重置
         print(f'与客户端{addr}的连接被终止！')
